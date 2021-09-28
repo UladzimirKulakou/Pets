05b11c131a56a6cb2eb90067c052556622624c2e
Uladzimir Kulakou <rombik9@ya.ru>
2021-09-28 19:09:15 +0300

    commit 1
    create project

---
+++ Pets/Pets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pets
+//
+//  Created by Uladzimir Kulakou on 9/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
