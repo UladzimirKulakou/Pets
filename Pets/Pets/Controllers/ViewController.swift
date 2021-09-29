7883f6a578e6c700379d0916c214c22e4ab85f5b
Uladzimir Kulakou <rombik9@ya.ru>
2021-09-30 00:01:10 +0300

    commit 3
    add Firebase

--- Pets/Pets/Controllers/ViewController.swift
+++ Pets/Pets/Controllers/ViewController.swift
@@ -11,8 +11,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let button = UIButton(type: .roundedRect)
+              button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
+              button.setTitle("Test Crash", for: [])
+              button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
+              view.addSubview(button)
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func crashButtonTapped(_ sender: AnyObject) {
+          let numbers = [0]
+          let _ = numbers[1]
+      }
+    
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         if Core.shared.isNewUser() {
